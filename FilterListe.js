a66453f3ad491ff7749156e092d97175330c30c5
Mohamed <m.tata@intech-sud.fr>
2021-01-04 11:28:45 +0100

    j

--- FilterListe.js
+++ FilterListe.js
@@ -1,12 +1,13 @@
 function myFunction() {
-    // Declare variables
+    // Declaration des variables
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
     ul = document.getElementById("myUL");
     li = ul.getElementsByTagName('li');
   
-    // Loop through all list items, and hide those who don't match the search query
+    // Passage en revue de tous les éléments de la liste et masquer ceux qui ne correspondent pas à la requête de recherche
+
     for (i = 0; i < li.length; i++) {
       a = li[i].getElementsByTagName("a")[0];
       txtValue = a.textContent || a.innerText;
