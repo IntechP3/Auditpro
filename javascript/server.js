232178acb08aabd327e44dbd3c3666d6afcfaaec
Nauwk <a.mathieu@intech-sud.fr>
2021-01-05 09:57:39 +0100

    t
    ht

--- javascript/server.js
+++ javascript/server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require('http'); /* Initilialisation serveur */
 const fs = require('fs');
 
 const hostname = '127.0.0.1';
@@ -6,7 +6,7 @@ const port = 3000;
 
 function defineContent(ContentType) { /* Fonction qui va définir le type de contenu à chaque appel de fichier */
 
-    switch (ContentType) {
+    switch (ContentType) { /* Fonction qui définit le type de contenu pour chaque fichier demandé */
         case 'css':
             return "text/css";
         case 'javascript':
@@ -40,24 +40,24 @@ function displayPages(Pages) { /* Fonction qui va récupérer la page demandée 
         case 'InfosMedico':
             return './html/InfosMedico.html';
         default:
-            return './html/error.html';
+            return 'Nothing';
     }
 }
 
 const server = http.createServer((request, response) => {
     let reqUrl = request.url.split('/');
 
-    if (request.url === `/${reqUrl[1]}/${reqUrl[2]}`) {
+    if (request.url === `/${reqUrl[1]}/${reqUrl[2]}`) { /* On demande de récupéerer les diférents fichiers dans les odssiers respectifs */
         fs.readFile(`./${reqUrl[1]}/${reqUrl[2]}`, (error, buffer) => {
 
-            response.writeHead(200, {"Content-Type": defineContent(reqUrl[1])});
-            response.write(buffer);
-            response.end();
+            response.writeHead(200, {"Content-Type": defineContent(reqUrl[1])}); /* Le fichier exise bien, on définit son type avec la fonction */
+            response.write(buffer); /* Stockage de l'info */
+            response.end(); /* Tout a bien fonctionné */
 
         });
 
     } else {
-        fs.readFile(displayPages(reqUrl[1]), (error, buffer) => {
+        fs.readFile(displayPages(reqUrl[1]), (error, buffer) => { /*Permet d'afficher les pages HTML dmeandées */
 
             response.writeHead(200, {"Content-Type": "text/html"});
             response.write(buffer);
@@ -68,5 +68,5 @@ const server = http.createServer((request, response) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Serveur ouvert à http://${hostname}:${port}/`);
+    console.log(`Serveur ouvert à http://${hostname}:${port}/`); /* Console.log pour afficher l'URL */
 });
