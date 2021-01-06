090ee984e67f52ba964632f616e4d7a016f8a240
Nicolas BAUDRY <n.baudry@intech-sud.fr>
2021-01-06 10:19:25 +0100

    commentaires

--- javascript/filter.js
+++ javascript/filter.js
@@ -8,13 +8,13 @@ function nameFilter() {
 
     // Passage en revue de tous les éléments de la liste et masquer ceux qui ne correspondent pas à la requête de recherche
 
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
+    for (i = 0; i < li.length; i++) { //Pour chaque element dans Listing
+        a = li[i].getElementsByTagName("a")[0]; //récupère les éléments 1 par 1
+        txtValue = a.textContent || a.innerText; //récupère le contenu de l'élément choisi
+        if (txtValue.toUpperCase().indexOf(filter) > -1) { //si le contenu de l'élément contient txtvalue, alors
+            li[i].style.display = ""; //mettre le display par défaut
         } else {
-            li[i].style.display = "none";
+            li[i].style.display = "none"; //sinon, le cacher
         }
     }
 }
